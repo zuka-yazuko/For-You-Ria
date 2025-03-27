c70036d2ffdb88c32547294cb6eb162a597a3963
zuka-yazuko <lunardiachmad@gmail.com>
2025-03-27 21:49:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("love").addEventListener("click", function() {
+    let message = document.getElementById("message");
+    message.innerText = "Semangat Ria, kalau harinya lagi buruk semoga bisa jadi lebih baik, Love u 💖";
+    message.style.opacity = "1";
+    message.style.transform = "translateY(10px)";
+});
